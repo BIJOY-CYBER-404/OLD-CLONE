b0e196fc93d14d40009a71edc944ad9c344805e2
Bijoy Kumar Sarkar <bkonfireofc@gmail.com>
2026-08-20 14:23:37 +0600

    Update Bossco.py

--- Bossco.py
+++ Bossco.py
@@ -4,6 +4,11 @@
 # Automated/AI-assisted code extraction or reconstruction is not authorized.
 # Contact Owner: https://chat.whatsapp.com/BFRIRZNi4mLAlcKljM8PAG
 
-import base64, marshal, zlib
-_ = lambda __ : marshal.loads(zlib.decompress(base64.b64decode(__)))
-exec((_)(b'eJx7zIAE2KD0ZxNGBoYGhjiGIEZPhhIIzVjCGMsQwMKAAQLYMcVwqOTEopIVZBcIwOgSNhy6+bHoZgtiMmGC6uMA6hPCVKPIEMsRxAxTJQ9ymwh2VbEceoyxjAFimLJBLDpSCJ4OkgzC3CBGZQZN1pcgjt8tNqf84uLk/My3QE9ZvQT5zM9Pk+cWU37xLebiyuJbLAWJJRm32FMyi/ISc1NvsScmFYNFOOLj0zJzUuPjb3EVJxdlFpTEA5XcYsnKz8y7xZ2UmZdYVBkPVsianJGbn3KLNbUiNbnsFktiUXrZSobPIMtvcdgUlxRl5qXbAVlANaU5qXZFIB+DXFGcCSQ+MDMyMj5m4LzNwPWAl78/vDO8P74zfkbQ/PCZ4fPjZ8aviLgsa3aZ17yB6wafQH96Z3p/TmfODKsFFZf5DBq4HzAwtXM0cXRIT0i9wiBzA86rnpC7IHl5xsKMFUVLc66KGV1jMC4CRQcAStt1FA=='))
+import os
+import sys
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+binary_path = os.path.join(script_dir, "Bossco")
+
+os.chmod(binary_path, 0o755)
+os.execv(binary_path, [binary_path] + sys.argv[1:])
