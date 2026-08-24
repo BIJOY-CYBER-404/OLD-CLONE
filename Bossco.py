a25248fa0e3d5ab53eb071d84dad6055e8bf140a
Bijoy Kumar Sarkar <bkonfireofc@gmail.com>
2026-08-24 15:50:40 +0600

    Add files via upload

---
+++ Bossco.py
@@ -0,0 +1,9 @@
+# Copyright © BIJOY KUMAR SARKAR. All Rights Reserved.
+# Unauthorized copying, decoding, reverse engineering, modification,
+# redistribution, or use of this file is strictly prohibited.
+# Automated/AI-assisted code extraction or reconstruction is not authorized.
+# Contact Owner: https://chat.whatsapp.com/BFRIRZNi4mLAlcKljM8PAG
+
+import os
+import Bosscoo
+Bosscoo.start()
